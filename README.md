46a6a0aa15aae3bec309d6234ee05d23ac0e51c1
wangzheng <wangzheng@ubox.cn>
2015-11-17 00:20:11 +0800

    v0.1
    OK, let’s try it.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # conf
-loading config file from running dir
+Loading config file from running dir.
+The config file should named .env.conf, formated as: 
+
+############### example start ###############
+mysql = user1:password1@tcp(127.0.0.1:3306)/db1?autocommit=true
+log_path = /var/log/abc.log
+############### example end   ###############
+
+So the conf will load these config items into key-value pair map, and 
+you will get them through conf.Get("mysql"), conf.Get("log_path").
--- init.go
+++ init.go
@@ -1,8 +1,66 @@
 package conf
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var configMap map[string]string
+
 /**
 *
  */
 func init() {
+	configMap = make(map[string]string)
+	load()
+}
 
+func Get(key string) (value string) {
+	value, _ = configMap[key]
+	return
+}
+
+func GetOrDefault(key, defaultValue string) string {
+	value, ok := configMap[key]
+	if ok {
+		return value
+	} else {
+		return defaultValue
+	}
+}
+
+/**
+*
+ */
+func load() {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		fmt.Errorf("Error get current dir: %s", err.Error())
+	}
+	fmt.Printf("Current dir: %s\n", dir)
+	f := fmt.Sprintf("%s/.env.conf", dir)
+	fmt.Printf("f: %s\n", f)
+	file, err := os.Open(f)
+	if err != nil {
+		log.Fatalf("Error load file: %s\n", err.Error())
+	}
+	reader := bufio.NewReader(file)
+	var key, value string
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err != io.EOF {
+				fmt.Errorf("Error read config file: %s", err.Error())
+			}
+			return
+		}
+		ss := strings.Split(line, "=")
+		key, value = strings.Trim(ss[0], " "), strings.Trim(ss[1], " ")
+		configMap[key] = value
+	}
 }
